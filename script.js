686c10e8a9b4d82ac90270f027f9cf21e3aa2962
Avinash Gavandhe <Avinash.Gavandhe@harbingergroup.com>
2022-10-22 08:39:27 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//Hoisting
+
+// var x = 10;
+// function getName(){
+//     console.log("Namaste JS");
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+//How functions works
+
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//   var x = 10;
+//   console.log(x);
+// }
+// function b() {
+//   var x = 100;
+//   console.log(x);
+// }
+
+//Undefined and not defined
+
+// var a = 2;
+
+
+//Scope chain 
+
+// function a (){
+// var b =10;
+// c();
+// function c(){
+//     console.log(b);
+// }
+// }
+// a();
+// // console.log(b);
+
+
+// let and const 
+
+let a =10;
+console.log(a);
+var b=10;
